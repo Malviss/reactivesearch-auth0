8d1b6b69d9cb0176fe58f51a431d694263407bb9
Malviss <a-lexys@hotmail.fr>
2021-03-18 08:17:44 +0900

    research-auth

---
+++ reactivesearch-auth0/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@appbaseio/reactivesearch/dist/css/style.min.css';
+
+import './index.css';
+import Routes from './Routes';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
+registerServiceWorker();
